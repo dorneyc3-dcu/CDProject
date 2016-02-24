6c13a3ec375b77595f82a67ffe6ff9a9ad53e2f3
Conor Dorney <conor.dorney3@mail.dcu.ie>
2016-02-24 22:52:56 +0000

    Initial commit

---
+++ app/src/androidTest/java/dcu/oscail/sda/dornec/cdproject/ApplicationTest.java
@@ -0,0 +1,13 @@
+package dcu.oscail.sda.dornec.cdproject;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
